6c87560e600dad5f7dc91cdffd281fb2129f891e
zhangxyai <zhangxy_buct@163.com>
2026-03-06 11:24:57 +0800

    deepseek-v32

--- sglang-server/docker_run.sh
+++ sglang-server/docker_run.sh
@@ -7,6 +7,7 @@ DOCKER_IMAGE=lmsysorg/sglang:v0.5.9
 docker run -itd --privileged=true --gpus all --ipc host --network host --name $DOCKER_CONTAINER_NAME\
     -v /lib/modules:/lib/modules \
     -v /dev:/dev \
+    -v $HOME/.cache:/root/.cache \
     -v $MODEL_PATH:/models \
     -v $SGLANG_CONFIG_PATH:/config \
     $DOCKER_IMAGE
---
+++ sglang-server/router_run.sh
@@ -0,0 +1,16 @@
+DOCKER_IMAGE=lmsysorg/sgl-model-gateway:v0.3.2
+CONTAINER_NAME=sgl-model-gateway
+ROUTER_PORT=8000
+MODEL_PATH=$HOME/workspace
+
+docker run -itd --privileged=true --restart unless-stopped --ipc host --network host --name $SGLANG_NAME \
+    -v /lib/modules:/lib/modules \
+    -v /dev:/dev \
+    -v $MODEL_PATH:/models \
+    $DOCKER_IMAGE \
+    --pd-disaggregation \
+    --host 0.0.0.0 \
+    --port $ROUTER_PORT \
+    --health-check-interval-secs 5 \
+    --policy round_robin \
+    --backend openai 
