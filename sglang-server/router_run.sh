8224687f97b9c6b6723f8ce3e3a73915b8a9c4a4
zhangxyai <zhangxy_buct@163.com>
2026-03-23 19:34:54 +0800

    fix router_run.sh

--- sglang-server/router_run.sh
+++ sglang-server/router_run.sh
@@ -1,17 +1,15 @@
 DOCKER_IMAGE=lmsysorg/sgl-model-gateway:v0.3.2
-CONTAINER_NAME=sgl-model-gateway
 ROUTER_PORT=8000
-MODEL_PATH=$HOME/workspace
+MODEL_PATH=$HOME/models
+HUGGINGFACE_DIR_NAME=DeepSeek-V3.2
+CONTAINER_NAME=sgl-model-gateway-$HUGGINGFACE_DIR_NAME
+
 
 prefill_urls=(
-    "http://10.119.194.202:30000"
-    "http://10.119.194.203:30000"
+    "http://172.26.168.32:30000"
 )
 decode_urls=(
-    "http://10.119.194.204:30000"
-    "http://10.119.194.205:30000"
-    "http://10.119.194.206:30000"
-    "http://10.119.194.207:30000"
+    "http://172.26.168.33:30000"
 )
 
 prefill_args=""
@@ -25,20 +23,20 @@ done
 echo "prefill_args: ${prefill_args}"
 echo "decode_args: ${decode_args}"
 
-docker run -itd --restart unless-stopped --gpus all \
+docker run -itd --restart unless-stopped \
     --ipc=host --privileged=true --network host --name $CONTAINER_NAME \
     -v /lib/modules:/lib/modules \
     -v /dev:/dev \
     -v $MODEL_PATH:/models \
     -v /root/.cache:/root/.cache \
     $DOCKER_IMAGE \
-        python -m sglang_router.launch_router \
+	--model-path /models/$HUGGINGFACE_DIR_NAME \
         --pd-disaggregation \
         ${prefill_args} \
         ${decode_args} \
         --host 0.0.0.0 \
+	--port $ROUTER_PORT \
         --max-concurrent-requests 4096 \
         --health-check-interval-secs 5 \
-        --policy round_robin \
-        --backend openai \
-        --port $ROUTER_PORT
+        --prefill-policy bucket \
+        --backend openai
